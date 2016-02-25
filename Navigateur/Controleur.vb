a0d18c61facc01db8abaed04db474bec29c3c670
LuisSolera <GitHub@mailalias.e4ward.com>
2016-02-25 08:09:10 -0500

    Test de connexion

---
+++ Navigateur/Controleur.vb
@@ -0,0 +1,26 @@
+﻿Imports TesterPile
+
+Public Class Controleur
+
+    Private Property _HistoriqueArriere As IPile
+    Private Property _HistoriqueAvant As IPile
+
+    Public Sub New()
+        Me._HistoriqueArriere = New Pile()
+        Me._HistoriqueAvant = New Pile()
+    End Sub
+
+    Public Sub New(ByVal HistoriqueArriere As IPile, ByVal HistoriqueAvant As IPile)
+        Me._HistoriqueAvant = HistoriqueAvant
+        Me._HistoriqueArriere = HistoriqueArriere
+    End Sub
+
+    Public Function Back() As String
+        Return Me._HistoriqueArriere.Depiler()
+    End Function
+
+    Public Function Forward() As String
+        Return Me._HistoriqueAvant.Depiler()
+    End Function
+
+End Class
---
+++ NavigateurTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("NavigateurTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NavigateurTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("d86bde75-f0e8-4673-98fd-c6520f2a4d92")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
